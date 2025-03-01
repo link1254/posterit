d0948b302e711ac6d2eaee71e4ebe0d2d7ea5767
link1254 <link12541254@gmail.com>
2025-03-01 16:10:49 +0100

    renvoie automatique du fichier winner crypté

--- decoder.py
+++ decoder.py
@@ -28,7 +28,7 @@ def decrypt_file_with_splited3_key(file_name, key1, key2, key3):
 def main():
    
     # 1) Demande à l'utilisateur le nom du fichier et les 3 parties de la clé
-    file_name = input("Entrez le nom du fichier à déchiffrer (ex: secret.txt.enc): ")
+    file_name = input("Entrez le nom du fichier à déchiffrer (laisser vide si le nom est winners_encrypted.txt): ")or "winners_encrypted.txt"
     key1 = input("Entrez la première partie de la clé (key1) : ")
     key2 = input("Entrez la deuxième partie de la clé (key2) : ")
     key3 = input("Entrez la troisième partie de la clé (key3) : ")
@@ -36,6 +36,10 @@ def main():
     # 2) Déchiffre le fichier avec la clé assemblée
     try:
         decrypt_file_with_splited3_key(file_name, key1, key2, key3)
+        # 3) Affiche le contenu du fichier déchiffré
+        decrypted_file_name = file_name[:-4] + "_decrypted.txt"
+        with open(decrypted_file_name, "r") as file:
+            print(file.read())
     except Exception as e:
         print(f"Une erreur est survenue lors du déchiffrement : {e}")
 
